ba32a9eff94b90980e5b67f9175420986392ee6c
Mohammed Anas <anas.dev@gmail.com>
2026-05-08 15:25:10 +0530

    final

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -40,7 +40,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`http://localhost:5050/record/`);
+      const response = await fetch(`https://fsd-employment.onrender.com/record/`);
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
         console.error(message);
@@ -55,7 +55,7 @@ export default function RecordList() {
 
   // This method will delete a record
   async function deleteRecord(id) {
-    await fetch(`http://localhost:5050/record/${id}`, {
+    await fetch(`https://fsd-employment.onrender.com/record/${id}`, {
       method: "DELETE",
     });
     const newRecords = records.filter((el) => el._id !== id);
